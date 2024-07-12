0c56391c439326fc16f7d4f4e5db7603f0b515fd
devanshtivari_antier <devansh.tiwari@antiersolutions.com>
2024-07-12 11:16:49 +0530

    updated build

--- build/components/Transaction/service.js
+++ build/components/Transaction/service.js
@@ -128,7 +128,10 @@ const TransactionService = {
                         status: 200,
                         error: false,
                         message: 'Data fetched Successfully',
-                        data: query
+                        data: {
+                            rows: query,
+                            totalPages: Math.ceil(query.length / 10)
+                        }
                     };
                 }
                 throw new Error('No data in database');
